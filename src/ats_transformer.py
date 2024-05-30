01d5c2d972dda1f65229d57207afbcc04da191c8
Abin (אבין ברגיס) <abinvarghese90@gmail.com>
2024-05-30 10:31:50 +0100

    added transformer

--- src/ats_transformer.py
+++ src/ats_transformer.py
@@ -1,3 +1,96 @@
 from extractor import ResumeParser
 from text_cleaner import TextCleaner
 
+from sentence_transformers import SentenceTransformer
+
+class ATSTransformer:
+    def __init__(self):
+        self.parser = ResumeParser()
+        self.cleaned_experience = None
+        self.cleaned_skills = None
+        self.jd = None
+
+    def load_data(self, resume_path, jd_path, skills_path):
+        """
+        Loads and processes the resume, job description, and skills data.
+        
+        Parameters:
+        -----------
+        resume_path : str
+            The path to the resume file.
+        jd_path : str
+            The path to the job description file.
+        skills_path : str
+            The path to the skills file.
+        """
+        # Load job description
+        with open(jd_path, 'r') as file:
+            self.jd = file.read()
+
+        # Load resume and skills into the parser
+        self.parser.load_resume(resume_path)
+        self.parser.load_job_description(jd_path)
+        self.parser.load_skills(skills_path)
+
+        # Extract experience section from the resume
+        experience = self.parser.extract_experience()        
+        # Clean the extracted experience text
+        self.clean_experience(experience)
+
+        # Extract skills from the resume section
+        skills = " ".join(self.parser.extract_skills())
+        # clean the extracted skills text
+        self.clean_skills(skills)
+
+    def clean_experience(self, experience):
+        """
+        Cleans the extracted experience text from the resume.
+        
+        Parameters:
+        -----------
+        experience : str
+            The raw experience text extracted from the resume.
+        """
+        cleaner = TextCleaner()
+        self.cleaned_experience = cleaner.clean_text(experience)
+
+    def clean_skills(self, skills):
+        """
+        Cleans the extracted skills text from the resume.
+        
+        Parameters:
+        -----------
+        skills : str
+            The raw skills text extracted from the resume.
+        """
+        cleaner = TextCleaner()
+        self.cleaned_skills = cleaner.clean_text(skills)
+
+    def compute_similarity(self):
+        model = SentenceTransformer('all-MiniLM-L6-v2')
+        cleaned_resume = self.cleaned_experience + self.cleaned_skills
+        sentences = [cleaned_resume, self.jd]
+        embeddings1 = model.encode(sentences[0])
+        embeddings2 = model.encode(sentences[1])
+        
+        similarity_score = model.similarity(embeddings1, embeddings2)
+
+        return similarity_score
+
+
+# Example usage:
+if __name__ == "__main__":
+    # Define the file paths
+    resume_path = 'resumes/resume_0.txt'
+    jd_path = 'job_descriptions/jd_2.txt'
+    skills_path = 'meta/skills.txt'
+    
+    # Create an instance of ATS
+    ats = ATSTransformer()
+    
+    # Load and process data
+    ats.load_data(resume_path, jd_path, skills_path)
+    
+    # Compute and print the similarity score
+    similarity_score = ats.compute_similarity()
+    print(f"The similarity score between the resume and job description is: {round(similarity_score.item() * 100, 2)}%")
